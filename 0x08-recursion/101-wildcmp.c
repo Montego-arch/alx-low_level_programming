e5c2c6049288f1aa82f38736afa4b333b046bbaa
Montego-arch <mmanuelmiles@gmail.com>
2022-06-22 02:12:12 -0700

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * wildcmp - compare strings
+ * wildcmp - Compare strings
  * @s1: pointer to string params
- * @s2: pointer to spring params
+ * @s2: pointer to string params
  * Return: 0
  */
 
@@ -14,7 +14,7 @@ if (*s1 == '\0')
 if (*s2 != '\0' && *s2 == '*')
 {
 return (wildcmp(s1, s2 + 1));
-{
+}
 return (*s2 == '\0');
 }
 
