b05137d33ea6a4d2e9749dd3a389393a6c2b05cb
Montego-arch <mmanuelmiles@gmail.com>
2022-09-05 07:45:23 -0700

    task 0

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - Returns the lenght of a string.
+ * @str: string
+ *
+ * Return: Lenght.
+ */
+int _strlen(char*s)
+{
+	int len = 0;
+
+	while (*s++)
+	     len++;
+
+	return (len);
+}
