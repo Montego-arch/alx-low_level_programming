f4587c008d17dc1775111e14ea86f8b6d546085a
Montego-arch <mmanuelmiles@gmail.com>
2022-09-05 10:57:40 -0700

    task 7

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * add - This function add two numbers
+ * @x: fist number
+ * @y: second number
+ * Author - Miles
+ *
+ * Return: return addition of two numbers
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * div - This function divide two numbers
+ * @x: fist number
+ * @y: second number
+ * Auhor - Miles
+ *
+ * Return: return division of two numbers
+ */
+int div(int x, int y)
+{
+	return (x/y);
+}
